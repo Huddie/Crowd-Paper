e8f63ec081e27004e77a2e945de0f0184b2e9ebf
Ehud Adler <ehudadler@Ehuds-MacBook-Pro.local>
2015-02-02 14:01:16 -0500

    Initial Commit

---
+++ Crowd Paper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crowd Paper
+//
+//  Created by Ehud Adler on 2/2/15.
+//  Copyright (c) 2015 Ehud Adler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Crowd Paper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Crowd Paper
+//
+//  Created by Ehud Adler on 2/2/15.
+//  Copyright (c) 2015 Ehud Adler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
