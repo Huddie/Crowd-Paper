8b2228c7f991487074a8bc5416c05664085d807b
Huddie <ehudadler@Ehuds-MacBook-Pro.local>
2015-03-13 13:59:51 -0400

    first commit

---
+++ Crowd Paper/ArticleCell.h
@@ -0,0 +1,13 @@
+//
+//  ArticleCell.h
+//  Crowd Paper
+//
+//  Created by Ehud Adler on 3/9/15.
+//  Copyright (c) 2015 Ehud Adler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArticleCell : UITableViewCell
+
+@end
---
+++ Crowd Paper/ArticlesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ArticlesViewController.h
+//  Crowd Paper
+//
+//  Created by Ehud Adler on 3/9/15.
+//  Copyright (c) 2015 Ehud Adler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArticlesViewController : UIViewController
+
+@end
